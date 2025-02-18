e0af964b55475e7e3bf9387d2e9d1dfefa1af24b
Ismail Shaikh <91933697+coddyismail@users.noreply.github.com>
2025-02-19 03:44:12 +0530

    db.php

--- db.php
+++ db.php
@@ -1,14 +1,17 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db_name = "chat_app";
+$database_url = getenv("${{ MySQL.MYSQL_URL }}"); //  DB URL from Railway
 
-$conn = new mysqli($host, $user, $pass, $db_name);
-if($conn->connect_error){
-    die("Connection failed: . $conn->connect_error");
-}
-else{
-    // echo "Connection Done";
+// Parse the URL to extract connection details
+$db = parse_url($database_url);
+$host = $db["host"];
+$user = $db["user"];
+$pass = $db["pass"];
+$dbname = ltrim($db["path"], "/");
+$port = $db["port"];
+
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
 }
 ?>
